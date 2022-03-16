a67ce164d2e141b972cf8b46fa3113e9b4286cbb
Happiness-James <gesarehjames@gmail.com>
2022-03-16 08:04:01 +0300

    commit

--- src/main.kt
+++ src/main.kt
@@ -1,27 +1,38 @@
 
 fun main(){
-stringArray("cat","dog","cow","gazzel")
-    grammatical(arrayOf("harare","mumbai","dodoma","akarta"))
-arithmetic()
-    var result = arrayOf("Happiness","James","Gesare")
-    println(result.contentToString())
+var result = product(arrayOf(3,5,2))
+    println(result)
+    var output = mixedArray(arrayOf(6,7.8F,"vote",9.8,56))
+    println(output)
+    var sum = arrayOfcharacter(arrayOf('a','g','f','e','k','i','o','u'))
+println(sum)
 }
-fun stringArray(name1: String,name2:String,name3: String,name4:String){
-    var four = arrayOf(name1,name2,name3,name4)
-    println(four.contentToString())
+fun product(numbers: Array<Int>):Int{
+   var num = 1
+   numbers.forEach { element ->
+       num*=element
+   }
+return num
 }
-fun grammatical(cities: Array<String>){
-    cities.forEach { country ->
-        println(country.capitalize())
+fun mixedArray(mixed: Array<Any>): Double{
+    var x = 0.00
+    mixed.forEach { number ->
+        if (number is Double || number  is Float)
+            x+=number.toString().toDouble()
+
     }
+    return x
+
 }
-fun arithmetic(){
-    var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
-    var sum = numbers[1]+numbers[4]
-    println(sum)
-    println(numbers[11])
-    println(numbers.sortedArray().contentToString())
-}
-fun names(name: Array<String>):Array<String>{
-        return name
+fun arrayOfcharacter(vowels: Array<Char>):Int{
+var vowel = 0
+    vowels.forEach { letter ->
+        if (letter=='a'|| letter=='e'||letter=='i'||letter=='o'||letter=='u')
+            vowel++
     }
+    return vowel
+
+}
+
+
+
